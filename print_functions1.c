2f2b496302f5fd9be761f3cda049cc9f1b06a9ea
hady444 <hadyhassan2312@gmail.com.com>
2023-08-22 20:04:08 +0000

    commit

--- print_functions1.c
+++ print_functions1.c
@@ -61,11 +61,18 @@ int printinteger(int num)
 	_putchar('0' + num % 10);
 	return (count + 1);
 }
+/**
+ * printbinary - Prints an integer to the standard output
+ * @n: The integer to be printed
+ *
+ * Return: The number of characters printed
+ */
 int printbinary(unsigned int n)
 {
 	int count = 0, i;
 
-	for (i = 0; i < 32; i++) {
+	for (i = 0; i < 32; i++)
+	{
 		_putchar((n & (1 << i)) ? '1' : '0');
 		count++;
 	}
