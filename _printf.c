7c0026223e0f2b600893fd98422b3430b27f719b
hady444 <hadyhassan2312@gmail.com.com>
2023-08-22 18:24:49 +0000

    commit

--- _printf.c
+++ _printf.c
@@ -9,7 +9,9 @@ int _printf(const char *format, ...)
 	va_list args;
 	int count = 0;
 
-	if (format == NULL)
+	if (!format || fotmat[0] == '%' && !format[1])
+		return (-1);
+	if (format[0] == '%' || format[1] == ' ' && !format[2])
 		return (-1);
 	va_start(args, format);
 	while (*format != '\0')
