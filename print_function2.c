c2c25b020fbb6ab1f28288d94aebbbde01c4999c
Rana Alaa <ranna.3laa@example.com>
2023-08-22 12:16:08 -0700

    0x11. C - printf

--- print_function2.c
+++ print_function2.c
@@ -1,43 +1,10 @@
-int _printf(const char *format, ...) {
+int _printf_percent(const char *format, ...) {
     va_list args;
     va_start(args, format);
     int charCount = 0;
     while (*format != '\0') {
         if (*format == '%') {
             format++; 
-            if (*format == '\0') {
-                break;
-            }
-            if (*format == 'c') {
-                char c = va_arg(args, int);
-                putchar(c);
-                charCount++;
-            } else if (*format == 's') {
-                char *str = va_arg(args, char *);
-                for (int i = 0; str[i] != '\0'; i++) {
-                    putchar(str[i]);
-                    charCount++;
-                }
-            } else if (*format == '%') {
-                putchar('%');
-                charCount++;
-            }
-        } else {
-            putchar(*format);
-            charCount++;
-        }
-        format++;
-    }
-    va_end(args);
-    return charCount;
-}
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-    int charCount = 0;
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++;
             if (*format == '%') {
                 putchar('%');
                 charCount++;
@@ -51,13 +18,13 @@ int _printf(const char *format, ...) {
     va_end(args);
     return charCount;
 }
-int _printf(const char *format, ...) {
+int _printf_string(const char *format, ...) {
     va_list args;
     va_start(args, format);
     int charCount = 0;
     while (*format != '\0') {
         if (*format == '%') {
-            format++;
+            format++; 
             if (*format == 's') {
                 char *str = va_arg(args, char *);
                 for (int i = 0; str[i] != '\0'; i++) {
@@ -74,13 +41,13 @@ int _printf(const char *format, ...) {
     va_end(args);
     return charCount;
 }
-int _printf(const char *format, ...) {
+int _printf_octal(const char *format, ...) {
     va_list args;
     va_start(args, format);
     int charCount = 0;
     while (*format != '\0') {
         if (*format == '%') {
-            format++;
+            format++; 
             if (*format == 'o') {
                 unsigned int value = va_arg(args, unsigned int);
                 char buffer[32];
@@ -99,16 +66,16 @@ int _printf(const char *format, ...) {
     va_end(args);
     return charCount;
 }
-int _printf(const char *format, ...) {
+int _printf_HEX(const char *format, ...) {
     va_list args;
     va_start(args, format);
     int charCount = 0;
     while (*format != '\0') {
         if (*format == '%') {
-            format++;
+            format++; 
             if (*format == 'x') {
                 unsigned int value = va_arg(args, unsigned int);
-                char buffer[32];
+                char buffer[32]; 
                 snprintf(buffer, sizeof(buffer), "%x", value);
                 for (int i = 0; buffer[i] != '\0'; i++) {
                     putchar(buffer[i]);
@@ -124,13 +91,13 @@ int _printf(const char *format, ...) {
     va_end(args);
     return charCount;
 }
-int _printf(const char *format, ...) {
+int _printf_reverse(const char *format, ...) {
     va_list args;
     va_start(args, format);
     int charCount = 0;
     while (*format != '\0') {
         if (*format == '%') {
-            format++;
+            format++; 
             if (*format == 'r') {
                 char *str = va_arg(args, char *);
                 int length = strlen(str);
