1dbe7b51b78b58447508310f69e5d1a1fbfdd562
hady444 <hadyhassan2312@gmail.com.com>
2023-08-25 16:03:54 +0000

    f

--- print_function2.c
+++ print_function2.c
@@ -88,7 +88,7 @@ int print_S(va_list ptr, params_t *params)
 		return (_puts(NULL_STRING));
 	for (; *str; str++)
 	{
-		if (*str > 0 && *str < 32 && *str >= 127)
+		if ((*str > 0 && *str < 32) || *str >= 127)
 		{
 			sum += _putchar('\\');
 			sum += _putchar('x');
